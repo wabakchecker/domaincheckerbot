4f0567b6c1a0dd2fcb0e3ecbe7b05d213ba79f00
wabakchecker <167243710+wabakchecker@users.noreply.github.com>
2024-04-21 04:12:56 +0800

    Update index.js

--- domainchecker copy/index.js
+++ domainchecker copy/index.js
@@ -13,7 +13,7 @@ app.listen(port, () => {
 });
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.BOT;
+const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
